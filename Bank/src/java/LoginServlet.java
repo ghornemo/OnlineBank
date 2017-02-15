931693e9239e738495a1a0687177f256a689ed81
Kene Udeh <kudeh@users.noreply.github.com>
2017-02-15 13:28:24 -0400

    Update LoginServlet.java

--- Bank/src/java/LoginServlet.java
+++ Bank/src/java/LoginServlet.java
@@ -45,7 +45,7 @@ public class LoginServlet extends HttpServlet {
         String status = "";
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        boolean success = SQL.loginRequest(email,password);
+        boolean success = SQL.loginRequest(email,password).trim();
         if(success)
             status = "Successful login";
         else
